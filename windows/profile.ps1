971caafd2b0b7bdc9fb1231b1855be0770f7957d
flespark <0860734llx@gmail.com>
2021-05-22 14:17:03 +0800

    windows pwshell profile

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -5,3 +5,13 @@ Set-PSReadLineOption -EditMode Emacs
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-PoshPrompt -Theme space
+
+function enProxy {
+	[Environment]::SetEnvironmentVariable("http_proxy", "http://127.0.0.1:8889", "User")
+	[Environment]::SetEnvironmentVariable("https_proxy", "http://127.0.0.1:8889", "User")
+}
+
+function deProxy {
+	[Environment]::SetEnvironmentVariable("http_proxy", $null, "User")
+	[Environment]::SetEnvironmentVariable("https_proxy", $null, "User")
+}
